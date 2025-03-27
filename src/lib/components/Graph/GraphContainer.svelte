15c35ff013b6dde5a812aee5ce08a785ccc64695
spenseay <spenseay@stanford.edu>
2025-03-26 17:27:58 -0700

    added axis.svelte but it has a warning

--- src/lib/components/Graph/GraphContainer.svelte
+++ src/lib/components/Graph/GraphContainer.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from 'svelte';
     import { adjustYAxisArrowForChart } from '$lib/utils/axisUtils.js';
+    import Axis from './Axis.svelte';
     
     // Props
     export let axes = { x: { start: "", end: "" }, y: { start: "", end: "" } };
@@ -24,25 +25,18 @@
     bind:this={containerRef}
     style="max-width: {maxWidth}">
     
-    <!-- X axis labels -->
-    <div class="axis-label x-axis-start">{axes.x.start}</div>
-    <div class="axis-label x-axis-end">{axes.x.end}</div>
+    <!-- Use the new Axis component for X and Y axes -->
+    <Axis 
+      type="x" 
+      start={axes.x.start} 
+      end={axes.x.end} 
+      {containerRef} />
     
-    <!-- Y axis labels -->
-    <div class="axis-label y-axis-start">{axes.y.start}</div>
-    <div class="axis-label y-axis-end">{axes.y.end}</div>
-    
-    <!-- Y axis arrow -->
-    <div class="axis-arrow y-axis-arrow">
-      <div class="arrow-head arrow-top"></div>
-      <div class="arrow-head arrow-bottom"></div>
-    </div>
-    
-    <!-- X axis arrow -->
-    <div class="axis-arrow x-axis-arrow">
-      <div class="arrow-head arrow-left"></div>
-      <div class="arrow-head arrow-right"></div>
-    </div>
+    <Axis 
+      type="y" 
+      start={axes.y.start} 
+      end={axes.y.end} 
+      {containerRef} />
     
     <!-- Slot for pins or other content -->
     <slot></slot>
@@ -58,125 +52,5 @@
       border: 2px solid #4c2c69;
       border-radius: 4px;
       background-color: white;
-    }
-    
-    /* Axis labels */
-    .axis-label {
-      position: absolute;
-      font-weight: bold;
-      color: #4c2c69;
-      z-index: 5;
-      font-size: 12px;
-      padding: 3px;
-      background-color: rgba(255, 255, 255, 0.7);
-      border-radius: 3px;
-      text-align: center;
-      max-width: 100px;
-    }
-    
-    /* X axis labels */
-    .x-axis-start {
-      left: 10px;
-      bottom: calc(50% + 15px);
-    }
-    
-    .x-axis-end {
-      right: 10px;
-      bottom: calc(50% + 15px);
-    }
-    
-    /* Y axis labels */
-    .y-axis-start {
-      left: 50%;
-      bottom: 10px;
-      transform: translateX(-50%);
-    }
-    
-    .y-axis-end {
-      left: 50%;
-      top: 10px;
-      transform: translateX(-50%);
-    }
-    
-    /* Y axis arrow */
-    .y-axis-arrow {
-      position: absolute;
-      pointer-events: none;
-      left: 50%;
-      width: 2px;
-      transform: translateX(-50%);
-      background-color: #4c2c69;
-      top: 30px;
-      bottom: 30px;
-    }
-    
-    /* X axis arrow */
-    .x-axis-arrow {
-      position: absolute;
-      pointer-events: none;
-      top: 50%;
-      left: 50px;
-      right: 50px;
-      height: 2px;
-      transform: translateY(-50%);
-      background-color: #4c2c69;
-    }
-    
-    /* Arrow heads */
-    .arrow-head {
-      position: absolute;
-      width: 0;
-      height: 0;
-      border-style: solid;
-    }
-    
-    .arrow-top {
-      top: -6px;
-      left: -4px;
-      border-width: 0 5px 8px 5px;
-      border-color: transparent transparent #4c2c69 transparent;
-    }
-    
-    .arrow-bottom {
-      bottom: -6px;
-      left: -4px;
-      border-width: 8px 5px 0 5px;
-      border-color: #4c2c69 transparent transparent transparent;
-    }
-    
-    .arrow-left {
-      left: -6px;
-      top: -4px;
-      border-width: 5px 8px 5px 0;
-      border-color: transparent #4c2c69 transparent transparent;
-    }
-    
-    .arrow-right {
-      right: -6px;
-      top: -4px;
-      border-width: 5px 0 5px 8px;
-      border-color: transparent transparent transparent #4c2c69;
-    }
-    
-    /* Responsive styles */
-    @media (min-width: 768px) {
-      .axis-label {
-        font-size: 13px;
-      }
-    }
-    
-    @media (max-width: 480px) {
-      .axis-label {
-        font-size: 10px;
-        max-width: 80px;
-      }
-      
-      .x-axis-start {
-        left: 5px;
-      }
-      
-      .x-axis-end {
-        right: 5px;
-      }
     }
   </style>
